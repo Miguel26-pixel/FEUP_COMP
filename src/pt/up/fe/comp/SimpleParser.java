52dbaee9001e5c3c2bb043434706fd2ddf9a2415
Bruno Mendes <brod56@sapo.pt>
2022-04-26 11:10:35 +0100

    Assert exception not null

--- src/pt/up/fe/comp/SimpleParser.java
+++ src/pt/up/fe/comp/SimpleParser.java
@@ -14,12 +14,12 @@ import pt.up.fe.specs.util.SpecsSystem;
 
 /**
  * Copyright 2022 SPeCS.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -46,12 +46,13 @@ public class SimpleParser implements JmmParser {
             System.out.println(result.getRootNode().toTree());
             return result;
         } catch (Exception e) {
-            var ex = TestUtils.getException(e, ParseException.class);
-            Token t = ex.getToken();
+            var exception = TestUtils.getException(e, ParseException.class);
+            assert exception != null;
+            Token t = exception.getToken();
             int line = t.getBeginLine();
             int column = t.getBeginColumn();
-            String message = ex.getMessage();
-            Report report = Report.newError(Stage.SYNTATIC,line,column,message,ex);
+            String message = exception.getMessage();
+            Report report = Report.newError(Stage.SYNTATIC, line, column, message, exception);
             return JmmParserResult.newError(report);
         }
     }
