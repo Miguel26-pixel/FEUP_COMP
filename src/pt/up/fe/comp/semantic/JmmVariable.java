eb7c76214e296528d9ef910266d504a707968551
David Preda <dtpreda30@gmail.com>
2022-04-28 18:28:29 +0100

    Change JmmVariable fields to final

--- src/pt/up/fe/comp/semantic/JmmVariable.java
+++ src/pt/up/fe/comp/semantic/JmmVariable.java
@@ -1,8 +1,8 @@
 package pt.up.fe.comp.semantic;
 
 public class JmmVariable {
-    public final String type;
-    public final String name;
+    private final String type;
+    private final String name;
 
     public JmmVariable(String type, String name) {
         this.type = type;
