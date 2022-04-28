acb4285b43d21e60d738d06a6131276cdd162d3b
David Preda <dtpreda30@gmail.com>
2022-04-28 17:46:24 +0100

    Revert "mend"
    This reverts commit 464fb64104b25a55706cd85ef6a5d7c91be8507c.

--- src/pt/up/fe/comp/semantic/JmmAnalyser.java
+++ src/pt/up/fe/comp/semantic/JmmAnalyser.java
@@ -4,21 +4,13 @@ import pt.up.fe.comp.jmm.analysis.JmmAnalysis;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.parser.JmmParserResult;
-import pt.up.fe.comp.semantic.visitors.ImportDeclarationVisitor;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class JmmAnalyser implements JmmAnalysis {
     @Override
     public JmmSemanticsResult semanticAnalysis(JmmParserResult parserResult) {
         SymbolTable symbolTable = null;
-        ImportDeclarationVisitor idv = new ImportDeclarationVisitor();
-        List<String> imports = new ArrayList<>();
-        
-        idv.visit(parserResult.getRootNode(), imports);
-        
         return new JmmSemanticsResult(parserResult, symbolTable, Collections.emptyList());
     }
 }
