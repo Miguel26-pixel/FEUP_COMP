d819815347816f92898f0e86bf71777268d75328
David Preda <dtpreda30@gmail.com>
2022-04-29 20:12:34 +0100

    Add LocalVariablesVisitor

---
+++ src/pt/up/fe/comp/semantic/visitors/LocalVariablesVisitor.java
@@ -0,0 +1,64 @@
+package pt.up.fe.comp.semantic.visitors;
+
+import pt.up.fe.comp.jmm.analysis.table.Symbol;
+import pt.up.fe.comp.jmm.analysis.table.Type;
+import pt.up.fe.comp.jmm.ast.AJmmVisitor;
+import pt.up.fe.comp.jmm.ast.JmmNode;
+import pt.up.fe.comp.jmm.parser.JmmParserResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class LocalVariablesVisitor extends AJmmVisitor<Map<String, List<Symbol>>, Boolean> {
+
+    public LocalVariablesVisitor() {
+        addVisit("Start", this::visitIntermediate);
+        addVisit("ClassDeclaration", this::visitIntermediate);
+        addVisit("RegularMethod", this::visitMethodDeclaration);
+        addVisit("MainMethod", this::visitMethodDeclaration);
+
+        setDefaultVisit((node, localVariables) -> true);
+    }
+
+    private Boolean visitIntermediate(JmmNode node, Map<String, List<Symbol>> localVariables) {
+        for (JmmNode child: node.getChildren()) {
+            this.visit(child, localVariables);
+        }
+
+        return true;
+    }
+
+    private Boolean visitMethodDeclaration(JmmNode methodDeclaration, Map<String, List<Symbol>> localVariables) {
+        String name = "";
+        List<Symbol> methodVariables = new ArrayList<>();
+
+        for (JmmNode child: methodDeclaration.getChildren()) {
+            if (child.getKind().equals("Identifier")) {
+                name = child.get("name");
+            }
+            else if (child.getKind().equals("MethodBody")) {
+
+                for (JmmNode line: child.getChildren()) {
+                    if (line.getKind().equals("VarDeclaration")) {
+                        JmmNode typeNode = line.getJmmChild(0), nameNode = line.getJmmChild(1);
+                        Type varType;
+
+                        if (typeNode.get("name").equals("intArray")) {
+                            varType = new Type("int", true);
+                        } else {
+                            varType = new Type(typeNode.get("name"), false);
+                        }
+
+                        methodVariables.add(new Symbol(varType, nameNode.get("name")));
+                    }
+                }
+
+            }
+        }
+
+        localVariables.put(name, methodVariables);
+
+        return true;
+    }
+}
