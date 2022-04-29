9ca4786e5e9cf94afd3c9ca3678505b6888239b2
David Preda <dtpreda30@gmail.com>
2022-04-29 03:14:34 +0100

    Update JmmAnalyser to initialize a SymbolTable

--- src/pt/up/fe/comp/semantic/JmmAnalyser.java
+++ src/pt/up/fe/comp/semantic/JmmAnalyser.java
@@ -4,13 +4,16 @@ import pt.up.fe.comp.jmm.analysis.JmmAnalysis;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.parser.JmmParserResult;
+import pt.up.fe.comp.semantic.visitors.ImportDeclarationVisitor;
+import pt.up.fe.comp.semantic.visitors.MethodDeclarationVisitor;
 
-import java.util.Collections;
+import java.util.*;
 
 public class JmmAnalyser implements JmmAnalysis {
     @Override
     public JmmSemanticsResult semanticAnalysis(JmmParserResult parserResult) {
-        SymbolTable symbolTable = null;
+        SymbolTable symbolTable = new JmmSymbolTable(parserResult);
+
         return new JmmSemanticsResult(parserResult, symbolTable, Collections.emptyList());
     }
 }
