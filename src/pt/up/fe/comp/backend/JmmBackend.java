5e74f0a07f1ccf0bfe4087331d349f86faa673e7
David Preda <dtpreda30@gmail.com>
2022-05-10 11:04:44 +0100

    Add class directive generation

--- src/pt/up/fe/comp/backend/JmmBackend.java
+++ src/pt/up/fe/comp/backend/JmmBackend.java
@@ -1,14 +1,47 @@
 package pt.up.fe.comp.backend;
 
+import org.specs.comp.ollir.ClassUnit;
+import org.specs.comp.ollir.Ollir;
+import org.specs.comp.ollir.parser.OllirParser;
 import pt.up.fe.comp.jmm.jasmin.JasminBackend;
 import pt.up.fe.comp.jmm.jasmin.JasminResult;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
+import pt.up.fe.comp.jmm.ollir.OllirUtils;
 
 import java.util.Collections;
 
 public class JmmBackend implements JasminBackend {
     @Override
     public JasminResult toJasmin(OllirResult ollirResult) {
-        return new JasminResult(ollirResult, "", Collections.emptyList());
+        String jasminCode = "";
+
+        jasminCode += getClassDirective(ollirResult.getOllirClass());
+        return new JasminResult(ollirResult, jasminCode, Collections.emptyList());
+    }
+
+    private String getClassDirective(ClassUnit ollirClass) {
+        String classDirective = ".class ";
+
+        if (ollirClass.isFinalClass()) {
+            classDirective += "final ";
+        }
+
+        if (ollirClass.isStaticClass()) {
+            classDirective += "static ";
+        }
+
+        if (ollirClass.getClassAccessModifier().toString().equals("DEFAULT")) {
+            classDirective += "public ";
+        } else {
+            classDirective += ollirClass.getClassAccessModifier().toString() + " ";
+        }
+
+        if (ollirClass.getPackage() != null) {
+            classDirective += ollirClass.getPackage() + "/";
+        }
+
+        classDirective += ollirClass.getClassName();
+
+        return classDirective;
     }
 }
