41408f68863742d5e9c70df202b62c1c63f64eea
David Preda <dtpreda30@gmail.com>
2022-05-11 08:25:19 +0100

    Start method definition header generation

--- src/pt/up/fe/comp/backend/MethodDefinitionGenerator.java
+++ src/pt/up/fe/comp/backend/MethodDefinitionGenerator.java
@@ -1,10 +1,21 @@
 package pt.up.fe.comp.backend;
 
+import org.specs.comp.ollir.Element;
 import org.specs.comp.ollir.Method;
+import org.specs.comp.ollir.Type;
 
 public class MethodDefinitionGenerator {
     public static String getConstructorDefinition(Method method, String superName) {
-        String constructorDefinition = ".method " + method.getMethodAccessModifier().toString() + " <init>()V\n";
+        String constructorDefinition = ".method ";
+
+        if (method.getMethodAccessModifier().toString().equals("DEFAULT")) {
+            constructorDefinition += "public ";
+        } else {
+            constructorDefinition += method.getMethodAccessModifier().toString().toLowerCase() + " ";
+        }
+
+        constructorDefinition += "<init>()V\n";
+
         constructorDefinition += "\taload_0\n";
 
         if (superName == null) {
@@ -21,7 +32,30 @@ public class MethodDefinitionGenerator {
     public static String getMethodDefinition(Method method) {
         String methodDefinition = "";
 
+        methodDefinition += getMethodHeader(method);
 
         return methodDefinition;
+    }
+
+    private static String getMethodHeader(Method method) {
+        String methodHeader = ".method ";
+
+        if (method.isFinalMethod()) {
+            methodHeader += "final ";
+        }
+
+        if (method.isStaticMethod()) {
+            methodHeader += "static ";
+        }
+
+        if (method.getMethodAccessModifier().toString().equals("DEFAULT")) {
+            methodHeader += "public ";
+        } else {
+            methodHeader += method.getMethodAccessModifier().toString().toLowerCase() + " ";
+        }
+
+        methodHeader += method.getMethodName() + "(";
+
+        return methodHeader;
     }
 }
